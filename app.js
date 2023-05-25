d0d4543aa5d9ea1850cc3f68569af036e8508963
nohsoobin <nohsoobin18@naver.com>
2023-05-25 11:42:39 +0900

    0525

--- app.js
+++ app.js
@@ -8,13 +8,9 @@ const app = express()
 app.listen(3000, () => {
     console.log("start! express server on port 3000")
 })
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-
-
 // request 와 response 라는 인자를 줘서 콜백 함수를 만든다.
 // localhost:3000 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
+app.use(express.static("node_modules/bootstrap/dist"))
 
 app.get('/', function(req,res) {
     res.sendFile(__dirname + "/view/index.html")
